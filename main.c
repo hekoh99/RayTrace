9ea278299cbcf5344dce625aca1504570178f99b
haeunko <hekoh99@naver.com>
2022-10-02 12:14:54 +0900

    render

--- main.c
+++ main.c
@@ -1,11 +1,37 @@
 #include "./mlx/mlx.h"
+#include <stdio.h>
 
-int		main(void)
+#include <stdio.h>
+
+int	main(void)
 {
-	void	*mlx;
-	void	*win;
+    int     i;
+    int     j;
+    double  r;
+    double  g;
+    double  b;
+    int     canvas_width;
+    int     canvas_height;
 
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 500, 500, "mlx_project");
-	mlx_loop(mlx);
+    // pixel of canvas
+    canvas_width = 256;
+    canvas_height = 256;
+
+    // render
+    printf("P3\n%d %d\n255\n", canvas_width, canvas_height);
+    j = canvas_height - 1;
+    while (j >= 0)
+    {
+        i = 0;
+        while (i < canvas_width)
+        {
+            r = (double)i / (canvas_width - 1);
+            g = (double)j / (canvas_height - 1);
+            b = 0.25;
+            printf("%d %d %d\n", (int)(255.999 * r), (int)(255.999 * g), (int)(255.999 * b));
+        	i++;
+        }
+    	j--;
+    }
+    return (0);
 }
